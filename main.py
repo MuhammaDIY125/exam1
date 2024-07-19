8abe60543d21c10e38a5bd425629ece6a772b91e
Muhammadiy <163087296+MuhammaDIY125@users.noreply.github.com>
2024-07-19 11:01:56 +0500

    tozalash

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # --------------------------------------------------------------------
 
@@ -7,7 +10,7 @@ st.title("Ma'lumotlar to'plami haqida")
 
 @st.cache_data
 def load_data(url):
-    df = pd.read_csv(url)
+    df = pd.read_csv(url).drop('Unnamed: 0', axis=1)
     return df
 
 df = load_data('zomato_new.csv')
@@ -24,3 +27,30 @@ st.write(df.dtypes)
 st.subheader("Ustunlardagi NaN lar soni")
 st.write(df.isnull().sum())
 st.write('Umumiy NaN lar soni:', df.isnull().sum().sum())
+
+# --------------------------------------------------------------------
+
+st.title("Tozalash")
+df2 = load_data('123.csv')
+st.dataframe(df2)
+st.write('Olib tashlangan ustunlar: url, address va phone')
+
+st.subheader("Yes/No to True/False")
+df['online_order'] = df['online_order'] == 'Yes'
+df['book_table'] = df['book_table'] == 'Yes'
+st.dataframe(df[['online_order', 'book_table']])
+st.write(df['book_table'].dtype)
+
+st.subheader("Clear 'rate'")
+df['rate'] = df['rate'].str.replace('/5', '').str.replace('[/5 -]', '', regex=True)
+df['rate'] = df['rate'].str.replace('NEW', '-1.0')
+df['rate'] = df['rate'].replace('', np.nan)
+df['rate'] = df['rate'].fillna(-2)
+df['rate'] = df['rate'].astype(float)
+st.dataframe(df)
+st.write('-1 - yangi')
+st.write('-2 - baholanmagan')
+st.write(df['rate'].dtype)
+
+st.subheader("Ustunlar tipi")
+st.write(df.dtypes)
